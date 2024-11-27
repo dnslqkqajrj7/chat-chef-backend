0dfdddb56618b98a8b3ee18ae84dece84bf7b395
dnslqkqajrj7 <eunhee2a@gmail.com>
2024-11-27 17:42:08 +0900

    env

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import * as dotenv from "dotenv";
+import path from "path";
+// env 설정
+const __dirname = path.resolve();
+dotenv.config({ path: __dirname + "/.env" });
+
 console.log('Hello world');
+console.log(process.env.OPENAI_API_KEY);
